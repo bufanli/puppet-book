229cc44fc7e7db0dc9958fc83fc473b9d6edfdf7
bufanli <bflbfl1980@163.com>
2019-05-09 03:35:38 -0400

    add cron job

---
+++ modules/puppet/files/pull-updates.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/bufali/puppet-book
+git pull && /usr/local/bin/papply
--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,20 @@ class puppet {
 		source => 'puppet:///modules/puppet/papply.sh',
 		mode => '0755',
 	}
+	file { '/usr/local/bin/pull-updates':
+		source => 'puppet:///modules/puppet/pull-updates.sh',
+		mode => '0755',
+	}
+	file { '/home/bufanli/.ssh/id_rsa':
+		source => 'puppet:///modules/puppet/id_rsa',
+		owner => 'bufanli',
+		mode => '0600',
+	}
+	cron { 'run-puppet':
+		ensure => present,
+		user => 'bufanli',
+		command => '/usr/local/bin/pull-updates',
+		minute => '*/10',
+		hour => '*',
+	}
 }
