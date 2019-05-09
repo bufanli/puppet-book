c61558349e8399e1abbf237b9b276f5b8a70aadd
bufanli <bflbfl1980@163.com>
2019-05-09 05:21:55 -0400

    add memcached service

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,10 +3,12 @@ node 'master'{
 		content => "hello,master\n",
 	}
 	include puppet
+	include memcached 
 }
 node 'slave'{
 	file { '/tmp/hello':
 		content => "hello,slave\n",
 	}
 	include puppet
+	include memcached 
 }
